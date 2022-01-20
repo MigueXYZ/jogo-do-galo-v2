0fb093db134bf64087f373b0f8cbafc52cfd87b8
MigueXYZ <Miguel553013>
2022-01-20 10:29:25 +0000

    1ºfase js

--- jogo.js
+++ jogo.js
@@ -1,15 +1,51 @@
-let player;
+let player=0;
 let mode;
 let table=[
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " ",
-    " "
+    [" "," "," "],
+    [" "," "," "],
+    [" "," "," "]
 ];
 
-console.log();
+function test(){
+    // loop the outer array
+    for (let i = 0; i < activities.length; i++) {
+        // get the size of the inner array
+        var innerArrayLength = activities[i].length;
+        // loop the inner array
+        for (let j = 0; j < innerArrayLength; j++) {
+            const input = prompt("Escolhe uma posição");
+            play(input);
+            console.log(table[i]," /");
+        }
+        console.log("\n");
+    }
+}
+
+
+function play(pos){
+    if(table[pos-1]==" "){
+        table[pos-1]=player;
+        changePlayer();
+    }
+    else{
+        alert("Casa indisponivel!");
+    }
+
+}
+
+function changePlayer(){
+    if(player==1){
+        player=0;
+    }
+    else{
+        player=1;
+    }
+}
+
+function whosPlaying(){
+    document.getElementById("player").innerHTML="Player " + (player+1);
+}
+
+function checkIfWin(){
+
+}
