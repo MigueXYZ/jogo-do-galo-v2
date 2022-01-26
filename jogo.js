0f02f01a11bbf36d887fe83498eba5c691de0ee2
MigueXYZ <miguelito.gomes.silva@gmail.com>
2022-01-26 11:55:17 +0000

    inicio de ai

--- jogo.js
+++ jogo.js
@@ -1,55 +1,85 @@
 var objG;
+var backup;
 function fazTudo(){
     do{
         posicao=parseInt(Math.random()*9);
     }while(objG.tabuleiro[posicao]!=-1);
     objG.tabuleiro[posicao]=parseInt(0+objG.player);
-    objG.player=!objG.player;
-
-
+    objG.player=!objG.player;0
     objG.desenha();
     document.write('<br>');
     if(objG.verifica()===true){
-        alert('vb');
         clearInterval(intGame);
     }
 }
 
-function inicia(obj){
+function inicia(obj,computador=0){
     objG=obj;
+    backup=JSON.parse(JSON.stringify(obj));
+    if(computador!=0){
+       objG.nivel=1;
+    }
 }
-function restartGame(obj){
-    objG.tabuleiro=obj.tabuleiro;
+
+function restartGame(){
+    objG.tabuleiro=JSON.parse(JSON.stringify(backup.tabuleiro));
     objG.player=!objG.player;
     objG.desenha();
+}
 
+async function checkModal(ativo){
+    if (!$('#myModal').is(':visible')) {
+        ativo=false;
+        restartGame(backup);
+    }
+    if(ativo===true){
+        setInterval(checkModal(ativo),100);
+    }
+
+}
+
+function simula(){
+    let aux;
+    let simulador;
+    for (i = 0; i < 9; i++) {
+        simulador = JSON.parse(JSON.stringify(objG.tabuleiro));
+        if (simulador.tabuleiro[i] == -1) {
+
+        }
+    }
 }
 
 function regPlay(pos){
+    if(objG.player==false && objG.nivel!=0){
+
+    }
     if(objG.tabuleiro[pos-1]==-1){
         objG.tabuleiro[pos-1]=parseInt(0+objG.player);
         objG.desenha();
         aux=objG.verifica();
-        console.log(aux);
+        console.log("aux: "+aux);
+        console.log("tabuleiro: "+objG.tabuleiro);
         if(aux==-1){
+            checkModal(true);
+
             $('#titulo').html('Empate');
             $('#myModal').modal('show');
-            restartGame(game);
         }
         else if(aux!==0){
+            checkModal(true);
+
             $('#titulo').html('Vitória');
             $('#corpo').html('Parabéns jogador '+(!aux+1));
             $('#myModal').modal('show');
-        }
+
+                    }
         objG.player=!objG.player;
         $('#playjogador').html('Jogador '+ parseInt(1+!objG.player));
-
-
     }
     else{
-        $("#myModal .modal-body").html="<span> Casa Indisponivel </span>";
-        $("#myModal .modal-footer").html=("<button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Fechar</button>");
-        $("#myModal").modal();
+        $('#titulo').html("Erro!")
+        $('#corpo').html("<span> Casa Indisponivel </span>");
+        $('#myModal').modal('show');
     }
 }
 
